a0573c8c2203fb2ecc28b8ffa02995ba41eff79b
Imandra McKenzie and Lex Wilkins <lex@>
2019-03-27 18:56:43 -0700

    score for the new player and computer are displayed

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,28 @@ function Player() {
 
 Player.prototype.rollDice = function() {
   var rollScore = Math.floor(Math.random() * 6);
-  // console.log(rollScore);
+  console.log(rollScore);
+  if (rollScore === 1) {
+    rollScore = 0;
+    }
   return rollScore;
 }
 
+/* Player.prototype.scoreCheck = function() {
+  var newRoll = this.rollDice();
+  if newRoll === 0 {
 
+  }
+} */
 
 
 
 var newPlayer = new Player();
 
+var compPlayer = new Player();
 
 $(document).ready(function(){
 
+  $('#player-score').text(newPlayer.score);
+  $('#comp-score').text(compPlayer.score);
 });
