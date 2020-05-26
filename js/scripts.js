396a627e576105b5b30a6d5a9d4b5d2c836a5390
Imandra <indigoimandra@gmail.com>
2020-05-26 15:01:12 -0700

    Add text that flashes w/computer turn score with auto roll

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,8 @@ Player.prototype.rollDice = function() {
 Player.prototype.turnCompScore = function() {
   this.tempScore = 0;
   this.compScore();
+  // Show computer's final score for the auto-roll
+  $("#comp-temp-score").text(this.tempScore);
   return this.score;
 }
 
@@ -63,37 +65,45 @@ function remove_styles(){
 function show_comp_roll() {
   // Get HTML head element
   var head = document.getElementsByTagName('HEAD')[0];
-
   // Create new link Element
   var link = document.createElement('link');
-
   // set the attributes for link element
   link.href = 'css/comp-role.css';
   link.rel = 'stylesheet';
   link.type = 'text/css';
-
   // Append link element to HTML head
   head.insertBefore(link, head.firstChild);
 
   // Wait 2 seconds and remove CSS stylehseet from head
-  setTimeout(remove_styles, 2000);
+  setTimeout(remove_styles, 1000);
 }
 
 
 
 $(document).ready(function(){
+  // Hide the winner and loser banners to start
   $("#winner").hide();
   $("#loser").hide();
-  hideDice();
 
+  // Display the default scores on the page for the user
   $('#player-score').text(newPlayer.score);
   $('#comp-score').text(compPlayer.score);
 
+  // Add text row to HTML for each computer roll to show computer logic to user
+  let comp_current = document.createElement('div');
+  let temp_id = document.getElementById('temp-text');
+  console.log(temp_id);
+  comp_current.innerHTML = "<p id='comp-roll-text' style='display:none'>The computer rolled \
+  <span id='comp-temp-score'></span> this turn</p>";
+  temp_id.appendChild(comp_current);
+
+  // Function for the user roll die button
   $("#roll-btn").click(function(event){
     event.preventDefault();
 
     var playerRoll = newPlayer.rollDice();
 
+    // If the user rolls a 1 it clears the user temporary score and displays information letting them know their turn ended
     if (playerRoll === 0) {
       $('#score').text("You rolled a 1! The computer has rolled.");
       newPlayer.tempScore = 0;
@@ -105,7 +115,7 @@ $(document).ready(function(){
       newPlayer.tempScore += playerRoll;
       $('#score').text(newPlayer.tempScore);
     }
-
+    // Depending on the user's current role it shows the proper die image on the page
     if (playerRoll === 0){
       $("#one").show()
       $("#two").hide();
@@ -159,23 +169,25 @@ $(document).ready(function(){
       $("#five").hide();
       $("#one").hide();
     }
-
+    // If the user's score is over 100 it hides the game and displays a winner banner
     if (newPlayer.score >= 100){
       $("#winner").show();
       $(".game").hide();
     }
-
+    // If the computer's score is over 100 it hides the game and displays a loser banner
     if (compPlayer.score >= 100){
       $("#loser").show();
       $(".game").hide();
     }
   });
 
+  // Function for the user hold button
   $("#hold-btn").click(function(event){
     event.preventDefault();
 
     var compScoreDisplay = document.querySelector("#comp-score");
 
+    // If the user's temporary score is more than 0 it adds that amount to their total score and resets the current roll details and images, then the computer rolls automatically
     if (newPlayer.tempScore !== 0){
       newPlayer.score += newPlayer.tempScore;
       $("#player-score").text(newPlayer.score);
